c8332d1b7a54fc6228ec3f982a9b641d8d469707
ouxianfei <ouxianfei@smail.nju.edu.cn>
2021-01-28 18:01:31 +0800

    update HUGE

--- apps/microbench/include/benchmark.h
+++ apps/microbench/include/benchmark.h
@@ -11,7 +11,7 @@ extern "C" {
 #define MB * 1024 * 1024
 #define KB * 1024
 
-#define REF_CPU    "i7-7700K @ 4.20GHz"
+#define REF_CPU    "i9-9900K @ 3.60GHz"
 #define REF_SCORE  100000
 
 #ifdef SETTING_TEST
@@ -29,46 +29,46 @@ extern "C" {
 #define REPEAT  1
 
 //                  size |  heap | time |  checksum
-#define QSORT_S {      100,   1 KB,      0, 0x08467105}
-#define QSORT_M {    30000, 128 KB,      0, 0xa3e99fe4}
-#define QSORT_L {   400000,1640 KB,  20546, 0x216f5851}
-#define QSORT_H { 12800000,  64 MB,1034000, 0x17f28691}
-#define QUEEN_S {        8,   0 KB,      0, 0x0000005c}
-#define QUEEN_M {       11,   0 KB,      0, 0x00000a78}
-#define QUEEN_L {       12,   0 KB,   4707, 0x00003778}
-#define QUEEN_H {       15,   1 MB,1024000, 0x0022c710}
-#define    BF_S {        2,  32 KB,      0, 0xa6f0079e}
-#define    BF_M {       25,  32 KB,      0, 0xa88f8a65}
-#define    BF_L {      180,  32 KB,  23673, 0x9221e2b3}
-#define    BF_H {     1360, 512 KB, 910000, 0xdb49fbde}
-#define   FIB_S {        2,   1 KB,      0, 0x7cfeddf0}
-#define   FIB_M {       23,  16 KB,      0, 0x94ad8800}
-#define   FIB_L {       91, 256 KB,  28318, 0xebdc5f80}
-#define   FIB_H {      305,  64 MB,1086000, 0x00000000}
-#define SIEVE_S {      100,   1 KB,      0, 0x00000019}
-#define SIEVE_M {   200000,  32 KB,      0, 0x00004640}
-#define SIEVE_L { 10000000,   2 MB,  39361, 0x000a2403}
-#define SIEVE_H {140000000,  32 MB,1010000, 0x0078bb07}
-#define  PZ15_S {        0,   1 KB,      0, 0x00000006}
-#define  PZ15_M {        1, 256 KB,      0, 0x0000b0df}
-#define  PZ15_L {        2,   2 MB,   4486, 0x00068b8c}
-#define  PZ15_H {        2, 110 MB,   4486, 0x00068b8c}
-#define DINIC_S {       10,   8 KB,      0, 0x0000019c}
-#define DINIC_M {       80, 512 KB,      0, 0x00004f99}
-#define DINIC_L {      128,   1 MB,  10882, 0x0000c248}
-#define DINIC_H {      628, 100 MB, 980000, 0x0004ba05}
-#define  LZIP_S {      128, 128 KB,      0, 0xe05fc832}
-#define  LZIP_M {    50000,   1 MB,      0, 0xdc93e90c}
-#define  LZIP_L {  1048576,   4 MB,   7593, 0x8d62c81f}
-#define  LZIP_H { 54218215, 110 MB, 505000, 0x9547d2f0}
-#define SSORT_S {      100,   4 KB,      0, 0x4c555e09}
-#define SSORT_M {    10000, 512 KB,      0, 0x0db7909b}
-#define SSORT_L {   100000,   4 MB,   4504, 0x4f0ab431}
-#define SSORT_H {  4000000, 110 MB,1060000, 0x37cf6f7a}
-#define   MD5_S {      100,   1 KB,      0, 0xf902f28f}
-#define   MD5_M {   200000, 256 KB,      0, 0xd4f9bc6d}
-#define   MD5_L { 10000000,  16 MB,  17239, 0x27286a42}
-#define   MD5_H {109999999, 110 MB, 219000, 0xbc1a5b43}
+#define QSORT_S {     100,   1 KB,     0, 0x08467105}
+#define QSORT_M {   30000, 128 KB,     0, 0xa3e99fe4}
+#define QSORT_L {  100000, 640 KB,  4404, 0xed8cff89}
+#define QSORT_H { 4000000,  16 MB,227620, 0xe6178735}
+#define QUEEN_S {       8,   0 KB,     0, 0x0000005c}
+#define QUEEN_M {      11,   0 KB,     0, 0x00000a78}
+#define QUEEN_L {      12,   0 KB,  4069, 0x00003778}
+#define QUEEN_H {      15,   0 KB,819996, 0x0022c710}
+#define    BF_S {       2,  32 KB,     0, 0xa6f0079e}
+#define    BF_M {      25,  32 KB,     0, 0xa88f8a65}
+#define    BF_L {     180,  32 KB, 16815, 0x9221e2b3}
+#define    BF_H {    1360,  32 KB,771535, 0xdb49fbde}
+#define   FIB_S {       2,   1 KB,     0, 0x7cfeddf0}
+#define   FIB_M {      23,  16 KB,     0, 0x94ad8800}
+#define   FIB_L {      91, 256 KB, 20168, 0xebdc5f80}
+#define   FIB_H {     300,   2 MB,775012, 0xe30a6f00}
+#define SIEVE_S {     100,   1 KB,     0, 0x00000019}
+#define SIEVE_M {  200000,  32 KB,     0, 0x00004640}
+#define SIEVE_L {10000000,   2 MB, 34823, 0x000a2403}
+#define SIEVE_H {80000000,  10 MB,301058, 0x00473fc6}
+#define  PZ15_S {       0,   1 KB,     0, 0x00000006}
+#define  PZ15_M {       1, 256 KB,     0, 0x0000b0df}
+#define  PZ15_L {       2,   2 MB,  5360, 0x00068b8c}
+#define  PZ15_H {       3,  64 MB,300634, 0x01027b4a}
+#define DINIC_S {      10,   8 KB,     0, 0x0000019c}
+#define DINIC_M {      80, 512 KB,     0, 0x00004f99}
+#define DINIC_L {     128, 680 KB,  8182, 0x0000c248}
+#define DINIC_H {     190,   2 MB,671978, 0x00014695}
+#define  LZIP_S {     128, 128 KB,     0, 0xe05fc832}
+#define  LZIP_M {   50000,   1 MB,     0, 0xdc93e90c}
+#define  LZIP_L { 1048576,   4 MB,  6795, 0x8d62c81f}
+#define  LZIP_H {31457280,  64 MB,199541, 0x1b859d76}
+#define SSORT_S {     100,   4 KB,     0, 0x4c555e09}
+#define SSORT_M {   10000, 512 KB,     0, 0x0db7909b}
+#define SSORT_L {  100000,   4 MB,  4002, 0x4f0ab431}
+#define SSORT_H { 3000000,  64 MB,322232, 0xeddbd9b6}
+#define   MD5_S {     100,   1 KB,     0, 0xf902f28f}
+#define   MD5_M {  200000, 256 KB,     0, 0xd4f9bc6d}
+#define   MD5_L {10000000,  10 MB, 15199, 0x27286a42}
+#define   MD5_H {64000000,  64 MB, 97148, 0x41ab4d60}
 
 #define BENCHMARK_LIST(def) \
   def(qsort, "qsort", QSORT_S, QSORT_M, QSORT_L, QSORT_H, "Quick sort") \
